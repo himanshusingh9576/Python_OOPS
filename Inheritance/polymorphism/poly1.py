f864f974e757707f8b08dd5b1c669c00985647b7
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-09-28 22:47:32 +0000

    done

--- Inheritance/polymorphism/poly1.py
+++ Inheritance/polymorphism/poly1.py
@@ -1,3 +1,25 @@
-print(1 + 3)
-print("Himanshu" + "singh") # concatenate
-print[1, 2, 3] + [4, 5, 6] # its merge the list
+# print(1 + 3)
+# print("Himanshu" + "singh") # concatenate
+# print[1, 2, 3] + [4, 5, 6] # its merge the list
+
+#complex number
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real, "i +",self.img,"j")
+
+    def add(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+num1 = Complex(1, 3)
+num1.showNumber()
+
+num2 = Complex(2, 4)
+num2.showNumber()
+
+num3 = num1.add(num2)
+num3.showNumber()
