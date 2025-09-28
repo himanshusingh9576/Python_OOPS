9c23129ec38c9c668e8a1e32d87e22f3cac2bc5d
Himanshu Kumar Singh <140483011+himanshusingh9576@users.noreply.github.com>
2025-09-28 22:22:53 +0000

    done

--- Inheritance/Multiplein.py
+++ Inheritance/Multiplein.py
@@ -13,6 +13,7 @@ print(c1.varA)"""
 
 
 ## SUPER() METHOD
+"""
 class Car:
     def __init__(self, type):
         self.type = type
@@ -33,4 +34,47 @@ class ToyotaCar(Car):
         super().stop()
 
 car1 = ToyotaCar("prius", "electric")
-print(car1.type)
+print(car1.type)  """
+
+
+# Class Method
+"""
+class Person:
+    name = "anonymous"
+
+    # def changeName(self, name):
+    #     self.singh = name
+    #     # Person.name = name
+    #     self.__class__.name = "Rahul" # instance method
+
+    @classmethod
+    def changeName(cls, name):
+        cls.singh = name
+        cls.name = name
+
+p1 = Person()
+p1.changeName("rahul")
+print(p1.singh)
+print(Person.name) """
+
+
+# Property decoraator
+class Student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+        
+
+    # def calcPercentage(self):
+    #     self.percentage = str((self.phy + self.math + self.chem)/3) + " %"
+    @property
+    def percentage(self) :
+        return str((self.phy + self.math + self.chem)/3) + " %"
+
+stu1 = Student(99, 98, 97)
+print(stu1.percentage)
+
+stu1.phy = 86
+print(stu1.percentage)
+print(stu1.phy)
